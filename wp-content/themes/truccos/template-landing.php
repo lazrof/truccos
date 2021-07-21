528b4a2e2ad5aacf0ee06c909df4f1331a8db457
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2021-07-21 11:55:00 -0500

    New theme and add gitignore

---
+++ wp-content/themes/truccos/template-landing.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* Template Name: Landing
+*/
+
+
+get_header(); ?>
+
+<main id="primary" class="site-main">
+		
+	<div class="page-section">
+		<div class="container">
+			<?php if ( have_posts() ) : ?>	
+				<?php while ( have_posts() ) : the_post();?>
+				
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+
+			<?php else: ?>
+				<h4>No posts were found.</h4>
+			<?php endif; ?>
+		</div>
+	</div>
+
+
+</main><!-- #main -->
+
+
+
+<?php get_footer(); ?>
